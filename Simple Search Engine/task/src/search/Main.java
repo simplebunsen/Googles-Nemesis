e191f7918ba004873ca692096a437ba57cb5a7ef
simplebunsen <really@dontdoxme.com>
2020-04-28 17:17:33 +0200

    Stage 2: Reading of people database and then asking n times about queries which are then found in the database.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -3,19 +3,49 @@ package search;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String[] entries;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String entries = scanner.nextLine();
-        String query = scanner.nextLine();
-        if(entries.contains(query)){
-            String[] seperateEntries = entries.split(" ");
-            for (int i = 0; i < seperateEntries.length; i++) {
-                if (seperateEntries[i].equals(query)){
-                    System.out.print((i+1) + " ");
-                }
-            }
-        }else{
-            System.out.println("Not found");
+
+        System.out.println("Enter the number of people in the database:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.printf("Enter all people on %d subsequent lines:\n", n);
+        entries = getEntriesFromInput(n);
+
+        System.out.println("Enter the number of search queries you will send:");
+        int q = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < q; i++) {
+            System.out.println("Enter data to search people:");
+            processQuery(scanner.nextLine());
         }
+    }
+
+    private static String[] getEntriesFromInput(int n) {
+        String[] entries = new String[n];
+        for (int i = 0; i < n; i++) {
+            entries[i] = scanner.nextLine();
+        }
+        return entries;
+    }
+
+    private static void processQuery(String input) {
+        input = input.toLowerCase();
+
+        boolean found = false;
+        for (String entry : entries) {
+            String s = entry.toLowerCase();
+            if (s.contains(input)) {
+                if(!found) System.out.println("The system has found people:");
+                found = true;
+                System.out.println(entry);
+            }
+        }
+        if (!found) System.out.println("No matching people found.");
     }
 }
