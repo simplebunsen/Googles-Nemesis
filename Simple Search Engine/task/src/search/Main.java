b8e2a09dfae05de8f1cf06bce92d144d4e5e37f0
simplebunsen <really@dontdoxme.com>
2020-04-28 15:13:34 +0200

    Stage 1: Implementation of our basic barebones string search.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,21 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String entries = scanner.nextLine();
+        String query = scanner.nextLine();
+        if(entries.contains(query)){
+            String[] seperateEntries = entries.split(" ");
+            for (int i = 0; i < seperateEntries.length; i++) {
+                if (seperateEntries[i].equals(query)){
+                    System.out.print((i+1) + " ");
+                }
+            }
+        }else{
+            System.out.println("Not found");
+        }
     }
 }
