2d5d35a204603e3d26e368040c16a579876dfbeb
simplebunsen <really@dontdoxme.com>
2020-04-29 12:19:33 +0200

    Stage 3 & 4: Added user menu and arg dependent import from file.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,5 +1,8 @@
 package search;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,21 +11,68 @@ public class Main {
     static String[] entries;
 
     public static void main(String[] args) {
+//        System.out.println("Enter the number of people in the database:");
+//        int n = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        System.out.printf("Enter all people on %d subsequent lines:\n", n);
+//        entries = getEntriesFromInput(n);
+        File input;
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--data":
+                    //TODO: catch case --data without filename
+                    input = new File(".\\" + args[++i]);
+                    try(Scanner fileScanner = new Scanner(input)){
+                        ArrayList<String> lines = new ArrayList<>();
+                        while (fileScanner.hasNextLine()){
+                            lines.add(fileScanner.nextLine());
+                        }
+                        //System.out.println(lines.toString());
+                        entries = lines.toArray(String[]::new);
+                    }catch (FileNotFoundException e) {
+                        System.out.println("Error: File not found");
+                    }
+            }
+        }
 
-        System.out.println("Enter the number of people in the database:");
-        int n = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.printf("Enter all people on %d subsequent lines:\n", n);
-        entries = getEntriesFromInput(n);
+        boolean running = true;
+        while (running) {
+            printMenuChoices();
+            switch (scanner.nextLine()) {
+                case "1":
+                    handleSearch();
+                    break;
+                case "2":
+                    printEntries();
+                    break;
+                case "0":
+                    System.out.println("Bye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Incorrect option! try again.");
+            }
+        }
+    }
 
-        System.out.println("Enter the number of search queries you will send:");
-        int q = scanner.nextInt();
-        scanner.nextLine();
+    private static void printMenuChoices() {
+        System.out.println("=== Menu ===\n" +
+                            "1. Find a person\n" +
+                            "2. Print all people\n" +
+                            "0. Exit");
+    }
 
-        for (int i = 0; i < q; i++) {
+    private static void handleSearch() {
             System.out.println("Enter data to search people:");
             processQuery(scanner.nextLine());
+    }
+
+    private static void printEntries() {
+        System.out.println("--- LIST OF PEOPLE ---");
+        for(String s : entries){
+            System.out.println(s);
         }
     }
 
