a1d6465127327b237af28eab28b5509dca94b0c2
simplebunsen <really@dontdoxme.com>
2020-04-29 14:56:39 +0200

    Stage 6: Added ANY, ALL, and NONE search algorithms. Code is sloppy and duplicated I know, gonna study some solutions to see what could have been done better.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -2,13 +2,16 @@ package search;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+
+
 
 public class Main {
 
-    static Scanner scanner = new Scanner(System.in);
-    static String[] entries;
+    public static Scanner scanner = new Scanner(System.in);
+    public static String[] entries;
+    public static Map<String, List<Integer>> invertedIndexMap;
+    public static SearchManager searchManager;
 
     public static void main(String[] args) {
 //        System.out.println("Enter the number of people in the database:");
@@ -17,24 +20,9 @@ public class Main {
 //
 //        System.out.printf("Enter all people on %d subsequent lines:\n", n);
 //        entries = getEntriesFromInput(n);
-        File input;
-        for (int i = 0; i < args.length; i++) {
-            switch (args[i]) {
-                case "--data":
-                    //TODO: catch case --data without filename
-                    input = new File(".\\" + args[++i]);
-                    try(Scanner fileScanner = new Scanner(input)){
-                        ArrayList<String> lines = new ArrayList<>();
-                        while (fileScanner.hasNextLine()){
-                            lines.add(fileScanner.nextLine());
-                        }
-                        //System.out.println(lines.toString());
-                        entries = lines.toArray(String[]::new);
-                    }catch (FileNotFoundException e) {
-                        System.out.println("Error: File not found");
-                    }
-            }
-        }
+
+        initialize(args);
+        searchManager = new SearchManager(entries, invertedIndexMap);
 
 
         boolean running = true;
@@ -57,6 +45,45 @@ public class Main {
         }
     }
 
+    private static void initialize(String[] args) {
+        File input;
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--data":
+                    //TODO: catch case --data without filename
+                    input = new File(".\\" + args[++i]);
+                    try(Scanner fileScanner = new Scanner(input)){
+                        ArrayList<String> lines = new ArrayList<>();
+                        while (fileScanner.hasNextLine()){
+                            lines.add(fileScanner.nextLine());
+                        }
+                        //System.out.println(lines.toString());
+                        entries = lines.toArray(String[]::new);
+                    }catch (FileNotFoundException e) {
+                        System.out.println("Error: File not found");
+                    }
+            }
+        }
+
+        invertedIndexMap = new HashMap<>();
+        for (int i = 0; i < entries.length; i++) {
+            String s = entries[i];
+            String[] split = s.split(" ");
+            for (String word : split) {
+                word = word.toLowerCase();
+                List<Integer> newList;
+                if(!invertedIndexMap.containsKey(word)){
+                    newList = new ArrayList<>();
+                }else{
+                    newList = invertedIndexMap.get(word);
+                }
+                newList.add(i);
+                invertedIndexMap.put(word, newList);
+            }
+        }
+        //System.out.println(invertedIndexMap.toString());
+    }
+
     private static void printMenuChoices() {
         System.out.println("=== Menu ===\n" +
                             "1. Find a person\n" +
@@ -65,8 +92,12 @@ public class Main {
     }
 
     private static void handleSearch() {
-            System.out.println("Enter data to search people:");
-            processQuery(scanner.nextLine());
+        System.out.println("Select a matching strategy: ALL, ANY, NONE");
+        searchManager.setMethod(scanner.nextLine());
+        if (searchManager.getMethod() == null) return;
+
+        System.out.println("Enter data to search people:");
+        searchManager.searchAndPrint(scanner.nextLine());
     }
 
     private static void printEntries() {
@@ -85,17 +116,172 @@ public class Main {
     }
 
     private static void processQuery(String input) {
-        input = input.toLowerCase();
+//        boolean found = false;
+//        for (String entry : entries) {
+//            String s = entry.toLowerCase();
+//            if (s.contains(input)) {
+//                if(!found) System.out.println("The system has found people:");
+//                found = true;
+//                System.out.println(entry);
+//            }
+//        }
+//        if (!found) System.out.println("No matching people found.");
+    }
+}
+interface SearchMethod {
+    void searchAndPrint(String input);
+}
 
-        boolean found = false;
-        for (String entry : entries) {
-            String s = entry.toLowerCase();
-            if (s.contains(input)) {
-                if(!found) System.out.println("The system has found people:");
-                found = true;
-                System.out.println(entry);
+class AllSearchMethod implements SearchMethod{
+
+    String[] entries;
+    Map<String, List<Integer>> invertedIndexMap;
+
+    public AllSearchMethod(String[] entries, Map<String, List<Integer>> invertedIndexMap) {
+        this.entries = entries;
+        this.invertedIndexMap = invertedIndexMap;
+    }
+
+    @Override
+    public void searchAndPrint(String input) {
+        input = input.toLowerCase();
+        String[] inputSplit = input.split(" ");
+        List<String> output = new ArrayList<>();
+        System.out.println(invertedIndexMap.toString());
+        System.out.println(Arrays.toString(inputSplit));
+        List<Integer> indexCandidates = new ArrayList<>(invertedIndexMap.get(inputSplit[0]));
+        //inverted index search.
+
+        for (String s : inputSplit) {
+            if (invertedIndexMap.containsKey(s)) {
+                indexCandidates.retainAll(invertedIndexMap.get(s));
+            } else {
+                output.clear();
+                break;
             }
         }
-        if (!found) System.out.println("No matching people found.");
+        for (Integer i : indexCandidates){
+            output.add(entries[i]);
+        }
+
+        if (output.isEmpty()) {
+            System.out.println("No matching people found, that hava all words of the query...");
+        } else {
+            System.out.println(String.valueOf(output.size()).concat(" persons have been found that has all" +
+                    "words in the query"));
+            for (String s : output) {
+                System.out.println(s);
+            }
+        }
+    }
+}
+
+class AnySearchMethod implements SearchMethod{
+
+    String[] entries;
+    Map<String, List<Integer>> invertedIndexMap;
+
+    public AnySearchMethod(String[] entries, Map<String, List<Integer>> invertedIndexMap) {
+        this.entries = entries;
+        this.invertedIndexMap = invertedIndexMap;
+    }
+
+    @Override
+    public void searchAndPrint(String input) {
+        input = input.toLowerCase();
+        String[] inputSplit = input.split(" ");
+        List<String> output = new ArrayList<>();
+        //inverted index search.
+        for (String s : inputSplit) {
+            if (invertedIndexMap.containsKey(s)) {
+                for (Integer i : invertedIndexMap.get(s)) {
+                    output.add(entries[i]);
+                }
+            }
+        }
+        if (output.isEmpty()) {
+            System.out.println("No matching people found...");
+        } else {
+            System.out.println(String.valueOf(output.size()).concat(" persons have been found"));
+            for (String s : output) {
+                System.out.println(s);
+            }
+        }
+    }
+}
+
+class NoneSearchMethod implements SearchMethod{
+    String[] entries;
+    Map<String, List<Integer>> invertedIndexMap;
+
+    public NoneSearchMethod(String[] entries, Map<String, List<Integer>> invertedIndexMap) {
+        this.entries = entries;
+        this.invertedIndexMap = invertedIndexMap;
+    }
+
+    @Override
+    public void searchAndPrint(String input) {
+        input = input.toLowerCase();
+        String[] inputSplit = input.split(" ");
+        List<String> output = new ArrayList<>(Arrays.asList(entries));
+        List<String> complement = new ArrayList<>();
+        //inverted index search.
+        for (String s : inputSplit) {
+            if (invertedIndexMap.containsKey(s)) {
+                for (Integer i : invertedIndexMap.get(s)) {
+                    complement.add(entries[i]);
+                }
+            }
+        }
+        output.removeAll(complement);
+        if (output.isEmpty()) {
+            System.out.println("No matching people found... (only lines containing the query exist)");
+        } else {
+            System.out.println(String.valueOf(output.size()).concat(" persons have been found to not have the query words"));
+            for (String s : output) {
+                System.out.println(s);
+            }
+        }
+    }
+}
+
+class SearchManager {
+    public SearchMethod getMethod() {
+        return method;
+    }
+
+    private SearchMethod method;
+
+    String[] entries;
+    Map<String, List<Integer>> invertedIndexMap;
+
+    public SearchManager(String[] entries, Map<String, List<Integer>> invertedIndexMap) {
+        this.entries = entries;
+        this.invertedIndexMap = invertedIndexMap;
+    }
+
+    public void setMethod(String method){
+        switch (method) {
+            case "ALL":
+                this.method = new AllSearchMethod(entries, invertedIndexMap);
+                break;
+            case "ANY":
+                this.method = new AnySearchMethod(entries, invertedIndexMap);
+                break;
+            case "NONE":
+                this.method = new NoneSearchMethod(entries, invertedIndexMap);
+                break;
+            default:
+                System.out.println("Unknown input for method");
+                this.method = null;
+        }
+    }
+
+    public void searchAndPrint(String input){
+        try {
+            method.searchAndPrint(input);
+        } catch (Exception e) {
+
+        }
     }
 }
